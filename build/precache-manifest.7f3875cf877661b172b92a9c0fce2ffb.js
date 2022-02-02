f23b4385433ca39e498ed1be3ff45b1214ade5f7
Himanshu Pandey <52776136+h-pandey@users.noreply.github.com>
2022-02-03 00:13:33 +0530

    Add files via upload

---
+++ build/precache-manifest.7f3875cf877661b172b92a9c0fce2ffb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0d8ce3e0f1f99b4db6c3379e80db67",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ebbd91f0eb311e81c132",
+    "url": "/monsters-rolodex/static/css/main.60d9ea58.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fef76e7449ec7d375dc7",
+    "url": "/monsters-rolodex/static/js/3.78ba26aa.chunk.js"
+  },
+  {
+    "revision": "ebbd91f0eb311e81c132",
+    "url": "/monsters-rolodex/static/js/main.7f32fd96.chunk.js"
+  },
+  {
+    "revision": "44d58ac42f2c240abb6e",
+    "url": "/monsters-rolodex/static/js/runtime-main.a07f12a2.js"
+  }
+]);
